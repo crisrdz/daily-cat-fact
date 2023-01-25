c1cef10c254ce063306543abe92af2b26232444b
Cristofer Rodríguez <cristofer.rodriguez.elgueta@gmail.com>
2023-01-24 21:43:01 -0300

    Cambio en target de links

--- src/Credits.jsx
+++ src/Credits.jsx
@@ -5,8 +5,8 @@ function Credits() {
     <div className="credits">
       <h6>Icons:</h6>
       <ul>
-        <li>License <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0 License</a></li>
-        <li>Project	<a href="https://fontawesome.com/">https://fontawesome.com/</a></li>
+        <li>License <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0 License</a></li>
+        <li>Project	<a href="https://fontawesome.com/" target="_blank">https://fontawesome.com/</a></li>
       </ul>
     </div>
   )
